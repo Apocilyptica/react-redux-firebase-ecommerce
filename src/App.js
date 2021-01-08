51971afe483ba7a425c3279cdf89f7294734332e
James Jager <jviper882000@gmail.com>
2021-01-08 04:56:47 -0500

    Built landing page and basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "./components/Header";
+import "./default.scss";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Header />
+      <div className="main">
+        <Homepage />
+      </div>
+    </div>
+  );
 }
 
 export default App;
