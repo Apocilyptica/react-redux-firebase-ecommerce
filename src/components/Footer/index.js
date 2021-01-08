c3d5cc63982e8ae78e5c405037724e333ed31661
James Jager <jviper882000@gmail.com>
2021-01-08 06:02:02 -0500

    react router / custom layouts / footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.scss";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div className="wrap">© James Jager</div>
+    </footer>
+  );
+};
+
+export default Footer;
