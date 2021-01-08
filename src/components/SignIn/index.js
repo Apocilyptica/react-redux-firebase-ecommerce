4483c6a6d3a2d4c95afd00811e257c29856cbd04
James Jager <jviper882000@gmail.com>
2021-01-08 09:26:26 -0500

    added registration and login with email and password

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,23 +1,60 @@
 import React, { Component } from "react";
 import "./styles.scss";
-import Buttons from "../Forms/Button";
-import { signInWithGoogle } from "../../firebase/utils";
+import { signInWithGoogle, auth } from "../../firebase/utils";
+
+import FormInput from "../Forms/FormInput";
+import Button from "../Forms/Button";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...initialState,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   handleSubmit = async (e) => {
     e.preventDefault();
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        ...initialState,
+      });
+    } catch (err) {
+      // console.log(err);
+    }
   };
 
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <div className="signin">
         <div className="wrap">
           <h2>Login</h2>
           <div className="formWrap">
             <form onSubmit={this.handleSubmit}>
+              <FormInput type="email" name="email" value={email} onChange={this.handleChange} placeholder="Email" />
+              <FormInput type="password" name="password" value={password} onChange={this.handleChange} placeholder="Password" />
+              <Button type="submit">LogIn</Button>
               <div className="socialSignin">
                 <div className="row">
-                  <Buttons onClick={signInWithGoogle}>Sign in with Google</Buttons>
+                  <Button onClick={signInWithGoogle}>Sign in with Google</Button>
                 </div>
               </div>
             </form>
