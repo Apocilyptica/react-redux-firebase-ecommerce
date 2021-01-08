7b63384079f33c13dd0613660f9d4453ce333a69
James Jager <jviper882000@gmail.com>
2021-01-08 08:26:46 -0500

    auth functions and user data

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAb-jrANsMQG02qOpWFPKAZguRMcl5MpCM",
+  authDomain: "ecommerce-website-bbf5f.firebaseapp.com",
+  projectId: "ecommerce-website-bbf5f",
+  storageBucket: "ecommerce-website-bbf5f.appspot.com",
+  messagingSenderId: "515764042162",
+  appId: "1:515764042162:web:da3e132715a74b746c0cab",
+};
