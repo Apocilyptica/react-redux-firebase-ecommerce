683245fe02079f8a656ecb9d59fcefa9e32ffce1
James Jager <jviper882000@gmail.com>
2021-01-13 17:10:47 -0500

    redux-persist

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -3,6 +3,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import createSagaMiddle from "redux-saga";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
@@ -13,4 +14,6 @@ export const middlewares = [thunk, sagaMiddleware, logger];
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
